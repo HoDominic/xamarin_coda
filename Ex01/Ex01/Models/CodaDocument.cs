a47a9fd93e88e3d79f05572e730a6e9190425667
HoDominic <dominic.ho@student.howest.be>
2021-08-10 13:11:37 +0200

    added model,repository,views

---
+++ Ex01/Ex01/Models/CodaDocument.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex01.Models
+{
+    public class CodaDocument
+    {
+
+        //properties : id, name, type
+
+
+
+        [JsonProperty(PropertyName = "id")]
+
+        public String Id { get; set; }
+
+
+        [JsonProperty(PropertyName = "name")]
+
+        public String Name { get; set; }
+
+
+        [JsonProperty(PropertyName = "type")]
+
+        public String Type { get; set; }
+
+
+
+
+    }
+}
---
+++ Ex01/Ex01/Models/CodaPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex01.Models
+{
+    public class CodaPage
+    {
+    }
+}
